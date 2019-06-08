1375e150e19c76d973386654a92c6f3cd8c463aa
yoann roche <el-rambo-poto@hotmail.fr>
2019-06-08 11:04:06 +0200

    Mise en place de l'architecture du coté serveur avec maven

---
+++ bibliotheque-serveur/bibliotheque-serveur-batch/src/main/java/fr/oc/projet/batch/App.java
@@ -0,0 +1,13 @@
+package fr.oc.projet.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
